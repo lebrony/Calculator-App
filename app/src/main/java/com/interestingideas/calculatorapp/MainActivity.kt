545111d4c3c8971ca8fba172a621ff47b7193132
lebrony <asandler778@gmail.com>
2022-02-27 01:39:29 +0300

    adding id's and oclick method on buttons

--- app/src/main/java/com/interestingideas/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/interestingideas/calculatorapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.interestingideas.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun numberEvent (view: View){
+
+        val btnSelect = view as Button
+
     }
 }
